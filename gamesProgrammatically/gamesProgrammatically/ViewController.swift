ef460121f9422da4d374986960ae332e305ad9a2
Benrise <benrise_x@mail.ru>
2022-04-13 18:20:32 +0300

    Storyboard reduced

---
+++ gamesProgrammatically/gamesProgrammatically/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  gamesProgrammatically
+//
+//  Created by Артём on 13.04.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+
+}
+
