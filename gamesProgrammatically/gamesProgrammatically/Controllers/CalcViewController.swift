3fb4dbca6519e74a6279a8c35eb79096f4c72665
Benrise <benrise_x@mail.ru>
2022-04-18 16:42:59 +0300

    Added func Stack Views with buttons

--- gamesProgrammatically/gamesProgrammatically/Controllers/CalcViewController.swift
+++ gamesProgrammatically/gamesProgrammatically/Controllers/CalcViewController.swift
@@ -33,6 +33,52 @@ class CalcViewController: UIViewController {
         return stackView
     }()
     
+    let funcViewStack1: UIStackView = {
+        let stackView = UIStackView()
+        stackView.axis = NSLayoutConstraint.Axis.horizontal
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        stackView.backgroundColor = .red
+        return stackView
+    }()
+    let funcViewStack2: UIStackView = {
+        let stackView = UIStackView()
+        stackView.axis = NSLayoutConstraint.Axis.horizontal
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        stackView.backgroundColor = .blue
+        return stackView
+    }()
+    let funcViewStack3: UIStackView = {
+        let stackView = UIStackView()
+        stackView.axis = NSLayoutConstraint.Axis.horizontal
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        stackView.backgroundColor = .purple
+        return stackView
+    }()
+    let funcViewStack4: UIStackView = {
+        let stackView = UIStackView()
+        stackView.axis = NSLayoutConstraint.Axis.horizontal
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        stackView.backgroundColor = .green
+        return stackView
+    }()
+    let funcViewStack5: UIStackView = {
+        let stackView = UIStackView()
+        stackView.axis = NSLayoutConstraint.Axis.horizontal
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        stackView.backgroundColor = .white
+        return stackView
+    }()
+    
+    let zeroViewStack: UIStackView = {
+        let stackView = UIStackView()
+        stackView.axis = NSLayoutConstraint.Axis.horizontal
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        stackView.backgroundColor = .white
+        return stackView
+    }()
+    
+    
+    
     let statusLabel: UILabel = {
         let label = UILabel()
         label.font = label.font.withSize(45)
@@ -48,6 +94,7 @@ class CalcViewController: UIViewController {
         label.textAlignment = .right
         return label
     }()
+    
     
     
     override func viewDidLoad() {
@@ -80,23 +127,61 @@ class CalcViewController: UIViewController {
         
         mainViewStack.addArrangedSubview(labelsViewStack)
         
-        labelsViewStack.leftAnchor.constraint(equalTo: mainViewStack.leftAnchor).isActive = true
-        labelsViewStack.rightAnchor.constraint(equalTo: mainViewStack.rightAnchor).isActive = true
-        labelsViewStack.topAnchor.constraint(equalTo: mainViewStack.topAnchor).isActive = true
-
-        labelsViewStack.widthAnchor.constraint(equalTo: view.widthAnchor).isActive = true
         labelsViewStack.heightAnchor.constraint(equalTo: mainViewStack.widthAnchor, multiplier: 1/4).isActive = true
     }
     func setupFunctional()
     {
         mainViewStack.addArrangedSubview(functionalViewStack)
         
-        functionalViewStack.leftAnchor.constraint(equalTo: mainViewStack.leftAnchor).isActive = true
-        labelsViewStack.rightAnchor.constraint(equalTo: mainViewStack.rightAnchor).isActive = true
-        labelsViewStack.topAnchor.constraint(equalTo: labelsViewStack.topAnchor).isActive = true
+
         
-        labelsViewStack.widthAnchor.constraint(equalTo: view.widthAnchor).isActive = true
-        labelsViewStack.heightAnchor.constraint(equalTo: mainViewStack.widthAnchor).isActive = true
+        functionalViewStack.addArrangedSubview(funcViewStack1)
+        functionalViewStack.addArrangedSubview(funcViewStack2)
+        functionalViewStack.addArrangedSubview(funcViewStack3)
+        functionalViewStack.addArrangedSubview(funcViewStack4)
+        
+        setupButton("AC", 1)
+        setupButton("⌫", 1)
+        setupButton("%", 1)
+        setupButton("/", 1)
+        
+        setupButton("7", 2)
+        setupButton("8", 2)
+        setupButton("9", 2)
+        setupButton("X", 2)
+        
+        setupButton("4", 3)
+        setupButton("5", 3)
+        setupButton("6", 3)
+        setupButton("-", 3)
+        
+        setupButton("1", 4)
+        setupButton("2", 4)
+        setupButton("3", 4)
+        setupButton("+", 4)
+        
+        
+    }
+    
+    func setupButton(_ title: String, _ tag: Int)
+    {
+        
+        let button = UIButton(type: .system)
+        button.setTitle(title, for: .normal)
+        button.setFontSize(fontSize: 30)
+        switch (tag)
+        {
+        case 1:
+            funcViewStack1.addArrangedSubview(button)
+        case 2:
+            funcViewStack2.addArrangedSubview(button)
+        case 3:
+            funcViewStack3.addArrangedSubview(button)
+        case 4:
+            funcViewStack4.addArrangedSubview(button)
+        default:
+            return
+        }
     }
 }
 
